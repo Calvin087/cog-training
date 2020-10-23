3cc5fb50c5aad406bf7ac6f91e915daa61d3079d
Calvin-T <calvin+1@sliame.com>
2020-10-23 07:21:50 +0200

    ups

--- Elegant-Automation/combining-techniques-files/pom_test.py
+++ Elegant-Automation/combining-techniques-files/pom_test.py
@@ -1,23 +1,30 @@
+import unittest
 from selenium import webdriver
 from pages.training_page_object import TrainingGroundPage
 from pages.trial_page import TrialPage
 
-# Test Set up
-browser = webdriver.Chrome()
+class TheTest(unittest.TestCase):
+    # Test Set up
 
-# Trial Page
-trial_page = TrialPage(driver=browser)
-trial_page.go()
-trial_page.stone_input.input_text('rock')
-trial_page.stone_button.click()
+    # Trial Page
+    def test_something(self):
+        browser = webdriver.Chrome()
+        trial_page = TrialPage(driver=browser)
+        trial_page.go()
+        trial_page.stone_input.input_text('rock')
+        trial_page.stone_button.click()
 
-input('continue?')
+        # input('continue?')
+    def test_something_else(self):
+        # Training Page
+        browser = webdriver.Chrome()
+        training_page = TrainingGroundPage(driver=browser)
+        training_page.go()
+        assert training_page.button1.text == 'Button12', 'problems on button1'
 
-# Training Page
-training_page = TrainingGroundPage(driver=browser)
-training_page.go()
-assert training_page.button1.text == 'Button1', 'problems on button1'
+        # input('continue?')
 
-input('continue?')
+        browser.quit()
 
-browser.quit()
+if __name__ == '__main__':
+    unittest.main()
