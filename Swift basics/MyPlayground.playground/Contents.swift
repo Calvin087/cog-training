978436708f26dadd6c92eda761e05bf622394328
Calvin-T <calvin+1@sliame.com>
2020-10-26 17:58:05 +0100

    starting swift

---
+++ Swift basics/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+// Variables are the var
+var age = 30
+var name = "Calvin T"
+
+// Constants are let...
+let air = "breathable"
+let myTime = -10
+
+// Not recommended
+// Long-hand way of writing the same thing
+// Is this what Typescript looks like?
+var age2 : Int = 30
+let hisName : String = "Sam"
