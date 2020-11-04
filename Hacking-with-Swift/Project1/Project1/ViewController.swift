433d86fbf7b67d09d0b34c0a5ee1faf48b92e6e2
Calvin-T <calvin+1@sliame.com>
2020-11-04 08:56:58 +0100

    swift Training

---
+++ Hacking-with-Swift/Project1/Project1/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Calvin T on 03/11/2020.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default // built in file system
+        let path = Bundle.main.resourcePath! // set resource path of app bundle (my compiled app directory)
+        let items = try! fm.contentsOfDirectory(atPath: path) // an array of all the files in the app
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is an image to load
+                // everythin after load is destroyed so we need to persist it outside the DidLoad()
+                pictures.append(item)
+            }
+        }
+     
+
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
--- Swift basics/low_intermediate.playground/Contents.swift
+++ Swift basics/low_intermediate.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 // #=============
 
 // Tuples and Sets
@@ -94,7 +95,7 @@ printOptional(text: "Hello")
 
 
 //------------------
-Optional
+//Optional chaining
 
 func albumReleased(year: Int) -> String? {
     switch year {
@@ -105,7 +106,165 @@ func albumReleased(year: Int) -> String? {
     }
 }
 
-let album = albumReleased(year: 2006)?.uppercased()
+let album = albumReleased(year: 2006)?.uppercased() //CHAINGING
 // Everything after the  mark will run IF ng before is valid.
 print("The album is \(album)")
 
+
+//------------------
+//Optional Coaliation/Coercian
+// This seems to be a little bit like ternary operators.
+// You DONT HAVE TO UNWRAP anything, as you provide a default of sorts.
+// No Crashes
+
+
+func albumReleased2(year: Int) -> String? {
+    switch year {
+    case 2006: return "Year 1"
+    case 2007: return "Year 2"
+    case 2008: return "Year 3"
+    default: return nil
+    }
+}
+
+let album2 = albumReleased2(year: 2010) ?? "Unknown"
+// Everything after the  mark will run IF ng before is valid.
+print("The album is \(album2)")
+// >> The album is Unknown
+
+// #=============
+// Enums
+// Gives us a list of types to check against. Anything other than those values, Swift will trigger an error.
+
+
+enum WeatherType {
+    case sun
+    case cloud
+    case rain
+    case wind
+    case snow
+}
+
+func getWeatherStatus(weather: WeatherType) -> String? {
+    if weather == WeatherType.sun {
+        return nil
+    } else {
+        return "bleh"
+    }
+}
+
+getWeatherStatus(weather: WeatherType.cloud)
+
+//------------------
+
+enum WeatherType1 {
+    case sun
+    case cloud
+    case rain
+    case wind(speed: Int)
+    case snow
+}
+
+func getWeatherStatus(weather: WeatherType1) -> String? {
+    switch weather {
+    case .sun:
+        return nil
+    case .wind(let speed) where speed < 10:
+        return "Meh"
+//    grab value inside of speed and check
+    case .cloud, .wind:
+        return "dislike"
+    case .rain, .snow:
+        return "hate"
+    }
+}
+
+getWeatherStatus(weather: .wind(speed: 20))
+
+// #=============
+// Struct
+// These are similar to objects, but you can also copy a newly made object
+
+struct Person {
+    var clothes: String
+    var shoes: String
+}
+
+let dave = Person(clothes: "Tees", shoes: "Sneaks")
+let sam = Person(clothes: "dungarees", shoes: "heels")
+
+print(dave.shoes)
+print(sam.clothes)
+
+var clone = dave
+clone.shoes = "kicks"
+
+print(clone)
+// >> Person(clothes: "Tees", shoes: "kicks")
+
+// #=============
+// Classes (Important in Cocoa Touch)
+// Similar to structs, but need to have init methods
+
+class Human {
+    
+    var clothes: String
+    var shoes: String
+    
+    init(clothes: String, shoes: String) {
+        self.clothes = clothes
+        self.shoes = shoes
+    }
+    
+    func sing() {
+        print("la la la")
+    }
+}
+
+class Clone: Human {
+    
+    override func sing() { // redefines the method that it has inherited from Human
+        print("Kill all humans")
+    }
+}
+
+class SuperSoldier: Human {
+    var powerLevel: Int
+    
+    init(clothes: String, shoes: String, powerLevel: Int) {
+        self.powerLevel = powerLevel
+        super.init(clothes: clothes, shoes: shoes) // passing values to parent class to initialise
+    }
+    
+    override func sing() {
+        print("Destroy all clones")
+    }
+}
+
+var vegeta = SuperSoldier(clothes: "Armour", shoes: "Saiyan Boots", powerLevel: 9000)
+var trunks = SuperSoldier(clothes: "Jacket", shoes: "Sneaks", powerLevel: 7000)
+var gohan = SuperSoldier(clothes: "Dress Shirt", shoes: "Brogues", powerLevel: 17000)
+
+// #=============
+// Polymorphism
+var fighters: [Human] = [vegeta, trunks, gohan]
+// Polymorphism on the Above Class
+
+for fighter in fighters {
+    print(fighter.sing())
+}
+
+// #=============
+// TypeCasting
+// as ! or as ?
+// NEED MORE INFO FOR THIS
+
+
+// #=============
+// Closures
+
+let vw = UIView()
+
+UIView.animate(withDuration: 0.5) {
+    vw.alpha = 0
+}
